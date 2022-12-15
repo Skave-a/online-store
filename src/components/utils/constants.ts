c2938be4a5de737cd1f58da4f6071dfacc22ee80
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-12-15 19:39:29 +0300

    feat: implementing 404Page

--- src/components/utils/constants.ts
+++ src/components/utils/constants.ts
@@ -1,2 +1,9 @@
 export const SOME_MESSAGE = 'Hello Main Page';
+
 export const LINK_TO_THE_COURSE = 'https://rs.school/js/';
+
+export const PAGE_NOT_FOUND = {
+  title: 'Oops!',
+  subTitle: '404 - The Page can not be found',
+  linkTitle: 'Go to homepage',
+};
