2ebdc7a03c5875419d4872cfffa0ebe5b32e1453
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-12-15 20:20:55 +0300

    feat: router, Basket and Main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from './components/modules/Footer/Footer';
 import { Header } from './components/modules/Header/Header';
+import Page404 from './components/Pages/Page404/Page404';
+import Main from './components/modules/Footer/Main/Main';
+import Basket from './components/Pages/Basket/Basket';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/basket" element={<Basket />} />
+        {/* <Route path="/anyPage" element={< AnyPage/>} /> */}
+        <Route path="*" element={<Page404 />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Pages/Basket/Basket.tsx
@@ -0,0 +1,7 @@
+import style from './Basket.module.css';
+
+function Basket() {
+  return <p className={style.basket}>Basket page</p>;
+}
+
+export default Basket;
