773f6856b124ad04b3b7a6ac2eba90147d6172aa
Skave-a <arrowtata@gmail.com>
2022-12-24 23:42:03 +0300

    feat: added inscription of failed search

--- src/components/modules/Cards/CardList.tsx
+++ src/components/modules/Cards/CardList.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import { FlowersType } from '../../types/types';
 import { CardItem } from './CardItem';
 
@@ -7,6 +8,18 @@ interface ICardList {
 }
 
 export const CardList = (props: ICardList) => {
+  if (!props.cards.length) {
+    return (
+      <Typography
+        variant="h2"
+        fontSize={25}
+        style={{ textAlign: 'center', color: '#006666' }}
+        fontFamily={`'Pacifico', cursive`}
+      >
+        Plants not found! Please change your search parameters
+      </Typography>
+    );
+  }
   return (
     <Grid container spacing={2} sx={{ maxWidth: '1000px' }}>
       {props.cards.map((item) => (
